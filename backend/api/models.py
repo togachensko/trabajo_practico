d46b0957aa2653a2fdeb8deef5a0a05598bb0cb7
AyeVillagra <89753772+AyeVillagra@users.noreply.github.com>
2021-11-16 18:30:08 -0300

    React

--- backend/api/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-
-class Course(models.Model):
-    name = models.CharField(max_length=30)
-    description = models.CharField(max_length=100)
--- pw-tup/src/components/Profile.js
+++
@@ -1,6 +0,0 @@
-
-const Profile = () => {
-  return <h2>Hola! Este es mi perfil</h2>
-}
-
-export default Profile
